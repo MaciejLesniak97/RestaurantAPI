f368ddd97a6fcb50e46975fd5763866297068601
MaciejLesniak97 <macieklesniak7@wp.pl>
2021-12-18 03:02:41 +0100

    Added: migration which changes column city and street from table Addresses

--- RestaurantAPI/Entities/RestaurantDbContext.cs
+++ RestaurantAPI/Entities/RestaurantDbContext.cs
@@ -33,6 +33,15 @@ namespace RestaurantAPI.Entities
                 .Property(d => d.Name)
                 .IsRequired();
 
+            modelBuilder.Entity<Address>()
+                .Property(a => a.City)
+                .IsRequired()
+                .HasMaxLength(50);
+
+            modelBuilder.Entity<Address>()
+                .Property(a => a.Street)
+                .IsRequired()
+                .HasMaxLength(50);
         }
 
         /// <summary>

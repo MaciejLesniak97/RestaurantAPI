ddacdc3147724d0acfd9bf66adab9baa55c068be
MaciejLesniak97 <macieklesniak7@wp.pl>
2021-12-21 03:05:25 +0100

    Added: automapper, mapping to Dto models

--- RestaurantAPI/Startup.cs
+++ RestaurantAPI/Startup.cs
@@ -26,16 +26,17 @@ namespace RestaurantAPI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddTransient<IWeatherForcastService, WeatherForcastService>();
             services.AddControllers();
             services.AddDbContext<RestaurantDbContext>();
             services.AddScoped<RestaurantSeeder>();
+            services.AddAutoMapper(this.GetType().Assembly);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env, RestaurantSeeder seeder)
         {
             seeder.Seed();
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
